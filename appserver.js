ba0d0c8cbdd7c571080931fd1f84c66abf3521a3
Jayachandra-UI <jaichandraios@gmail.com>
2022-05-20 22:47:24 +0530

    fixes for test

--- appserver.js
+++ appserver.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/list', function (req, res) {
+  res.send('open app Hello World!');
 });
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
